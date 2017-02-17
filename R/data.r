6c400554813b870115b4138aa6ad7ea5f4fc25e6
Eric Hare <erichare@iastate.edu>
2017-02-17 22:36:57 +0000

    version 0.1.3

--- R/data.r
+++ R/data.r
@@ -2,7 +2,7 @@
 #' 
 #' @name sequences
 #' @title peptide sequence data
-#' @description available through biovis redesign contest 2013, see http://www.biovis.net/year/2013/info/redesign-contest
+#' @description sequences showing the amino acid usage in the adenylate kinase lid available through biovis redesign contest 2013, see \url{http://biovis.net/year/2013/info/redesign-contest}
 #' published in Wong, B. Nat Methods 7, 889 (2011)
 #' @docType data
 #' @usage data(sequences)
@@ -11,6 +11,7 @@ NULL
 #' alphabet polygon data
 #' 
 #' The alphabet polygon data is based on the font Helvetica. Upper and lower case letters are included as well as digits.
+#' Other alphabet data are `alphabet_comic` (based on Comic Sans MS), `alphabet_xkcd` (based on the xkcd font),  and `alphabet_braille` (based on Swell Braille)
 #' 
 #' @name alphabet
 #' @title alphabet polygon data
@@ -18,6 +19,25 @@ NULL
 #' @docType data
 #' @usage data(alphabet)
 NULL
+
+#' @rdname alphabet
+#' @name alphabet_comic
+#' @docType data
+#' @usage data(alphabet_comic)
+NULL
+
+#' @rdname alphabet
+#' @name alphabet_xkcd
+#' @docType data
+#' @usage data(alphabet_xkcd)
+NULL
+
+#' @rdname alphabet
+#' @name alphabet_braille
+#' @docType data
+#' @usage data(alphabet_braille)
+NULL
+
 
 #' Amino acid properties
 #' 
