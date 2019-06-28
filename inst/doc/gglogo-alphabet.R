8c4469871af6910163011b60e2b7d0ec881c5496
Eric Hare <eric@oaiti.org>
2019-06-28 13:20:03 +0000

    version 0.1.4

--- inst/doc/gglogo-alphabet.R
+++ inst/doc/gglogo-alphabet.R
@@ -1,10 +1,8 @@
 ## ----setup, echo=FALSE---------------------------------------------------
-
 knitr::opts_chunk$set(fig.width = 6,
-                    fig.height = 4,
-                    fig.align='center',
-                    dev = "png")
-
+                      fig.height = 4,
+                      fig.align='center',
+                      dev = "png")
 
 ## ---- message=FALSE------------------------------------------------------
 library(ggplot2)
@@ -13,8 +11,9 @@ data(alphabet)
 head(alphabet)
 
 ## ------------------------------------------------------------------------
-qplot(x, y, geom="polygon", data=alphabet) + 
-  facet_wrap(facets=~group, ncol=13) +
+ggplot(data = alphabet, aes(x = x, y = y)) +
+  geom_polygon() +
+  facet_wrap(~group, ncol = 13) +
   coord_equal()
 
 ## ---- warning=FALSE------------------------------------------------------
@@ -22,24 +21,27 @@ alphas <- c(LETTERS, letters, 0:9)
 alphabet_garamond <- createPolygons(alphas, font="Garamond")
 alphabet_garamond$group <- factor(alphabet_garamond$group, levels=alphas)
 
-qplot(x,y, geom="polygon", data=alphabet_garamond,  group="pathGroup") +
-  facet_wrap(facets=~group, ncol=13)
+ggplot(alphabet_garamond, aes(x = x, y = y, group = pathGroup)) +
+  geom_polygon() +
+  facet_wrap(~group, ncol = 13)
 
 ## ---- warning=FALSE------------------------------------------------------
 alphas <- c(LETTERS, letters, 0:9)
 alphabet_helvetica <- createPolygons(alphas, font="Helvetica")
 alphabet_helvetica$group <- factor(alphabet_helvetica$group, levels=alphas)
 
-qplot(x,y, geom="polygon", data=alphabet_helvetica) +
-  facet_wrap(facets=~group, ncol=13)
+ggplot(alphabet_helvetica, aes(x = x, y = y)) +
+  geom_polygon() +
+  facet_wrap(~group, ncol = 13)
 
 ## ---- warning=FALSE------------------------------------------------------
 alphas <- c(LETTERS, letters, 0:9)
 alphabet_comic <- createPolygons(alphas, font="Comic Sans MS", scale=TRUE)
 alphabet_comic$group <- factor(alphabet_comic$group, levels=alphas)
 
-qplot(x,y, geom="polygon", data=alphabet_comic,  group="pathGroup") +
-  facet_wrap(facets=~group, ncol=13)
+ggplot(alphabet_comic, aes(x = x, y = y, group = pathGroup)) +
+  geom_polygon() +
+  facet_wrap(~group, ncol = 13)
 
 ## ------------------------------------------------------------------------
 data(sequences)
@@ -50,5 +52,4 @@ ggplot(data=ggfortify(sequences, seq="peptide", treatment="class")) +
             alpha = 0.8) +
   facet_wrap(~position) +
   scale_fill_brewer(palette="Paired")
-
 
